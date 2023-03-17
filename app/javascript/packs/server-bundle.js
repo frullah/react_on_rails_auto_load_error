22bf24782e951430aad267efb4505903de0c1cae
Fajarullah <frullah12@gmail.com>
2023-03-17 07:41:54 +0700

    enable RoR auto load
    make HelloWorld component auto-loadable

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,8 +1,2 @@
-import ReactOnRails from 'react-on-rails';
-
-import HelloWorld from '../bundles/HelloWorld/components/HelloWorldServer';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  HelloWorld,
-});
+// import statement added by react_on_rails:generate_packs rake task
+import "./server-bundle-generated.js"
